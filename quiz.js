f5e294cc18ed65a3ee90ab984846cc6a5bfa970f
Krista <krista.coding@gmail.com>
2020-09-21 21:43:43 -0400

    changes'

--- quiz.js
+++ quiz.js
@@ -1,13 +1,43 @@
-// Assignment Code 
-var startQuiz = document.getElementById("#startquiz");
-var startQuizBtn = document.getElementById("#startquizbtn");
+// Questions Array
+var questionType = [
+    {
+        question : "The condition in an if / else statement is enclosed within ____", 
+        options : ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer : "Parenthese",
+    },
+    {
+        question : "Arrays in JavaScript can be used to store _____.", 
+        options: ["Number and Strings", "Other Arrays", "Booleans", "All of the Above"], 
+        answer : "All of the Above",
+    },
+    {
+        question : "String Values must be enclosed within _________ when being assigned to variables", 
+        options : ["Quotes", "Curly Brackets", "Parentheses","Commas"],
+        answer : "Quotes",
+    },
+    {
+        question : "A very useful tool used during development and debugging for printing content to the debugger is _________.", 
+        options : ["JavaScript", "Console Log", "Terminal/Bash", "For Loops"],
+        answer : "Console Log",
+    },
+    {
+        question : "Commonly used data types do not include: _______", 
+        options : ["Numbers", "Booleans", "Alerts", "Strings"],
+        answer : "Alerts",
+    }
+];
 
-var quizbody = document.getElementById("#quiz");
-var questionSelect = document.getElementById("#questionSelect");
-var options = document.getElementById("#options");
+
+// Assignment Code 
+var startQuiz = document.getElementById("startQuiz");
+var startQuizBtn = document.getElementById("startquizbtn");
+
+var quizbody = document.getElementById("quiz");
+var questionSelect = document.getElementById("questionSelect");
+var options = document.getElementById("options");
 var listCreate = document.createElement("ul");
 
-var timer = document.getElementById("#timer");
+var timer = document.getElementById("timer");
 var timeLeft = 60;
 var timestart = 0;
 var penalty = 10;
@@ -18,13 +48,10 @@ var penalty = 10;
 
 function quiz() {
 
-    startQuiz.style.display = 'none';
-    display();
-
     // Timer
     timerInterval = setInterval(function () {
         timeLeft--;
-        timer.textContent = "Timer: " + timeLeft;
+        timer.textContent = timeLeft;
 
         if (timeLeft === 0) {
             clearInterval(timerInterval);
@@ -32,34 +59,40 @@ function quiz() {
         }
     }, 1000);
 
-    //render(generateQuestions);   
+    // clear contents
+
+    startQuiz.style.display = 'none';
+
+    generateQuestions;
 
 }
 
 
 // Prompts the Question
-function generateQuestions() {
+function generateQuestions(questionIndex) {
+
+    newDiv.innerHTML = "";
+    newList.innerHTML = "";
+
+    // Loop through array of questions
     for (let i = 0; i < questionType.length; i++) {
-
-        var questionSelect = document.createElement(h1);
-        questionSelect.textContent(questionType.question[i]);
-
-        var getOptions = document.createElement(button);
-        getOptions.addClass("btn btn-outline-info");
-        getOptions.textContent(questionType.options[i]);
-        askquestion.append(getOptions)
+        var userQuestion = questionType[i].question;
+        var userOptions = questionType[i].options;
+        questionsDiv.textContent = UserQuestion;
     }
 
+    // New format for questions
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        var listCreate = document.createElement()
+        newDiv.appendChild();
+        newList.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
 }
 
-// Checks if correct
-function correctAnswer(answer) {
-    if (answer === questionType[i].answer) {
-        timeLeft += 10;
-    }
-    else (answer !== questionType[i].answer)
-    timeLeft -= 10;
-}
+
 
 // Add event listener to start quiz 
-startQuizBtn.addEventListener("click", quiz());
+startQuizBtn.addEventListener("click", quiz);
